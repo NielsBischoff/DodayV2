fecf50d0699a1a5b7ab74b247fb91966df1a3614
nielsbischoff <niels.bischoff@gmail.com>
2015-06-06 15:55:30 -0700

    auth fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,9 +40,9 @@ class UsersController < ApplicationController
     redirect_to users_path #users_path is an alias to a route prefix defined in our routes
   end
 
-  private
+private
   def user_params
-    params.require(:user).permit(:name, :email, :password_digest)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end # of users_controller
